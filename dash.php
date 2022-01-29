f665fa511a8b26d88c18934ea2dc4681fb966007
Olubusolami-R <78950241+Olubusolami-R@users.noreply.github.com>
2022-01-28 23:20:55 -0800

    Add files via upload

---
+++ dash.php
@@ -0,0 +1,60 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="dash.css">
+    </head>
+    <body>
+        <div class="wrapper">
+            <div class="sidebar">
+                <h2>Sidebar</h2>
+                <ul>
+                    <li><a href="#">Dashboard</a></li>
+                    <li><a href="#">Student Registration</a></li>
+                    <li><a href="#">Course Registration</a></li>
+                    <li><a href="#">Result</a></li>
+                </ul>
+            </div>
+            <div class="main_content">
+
+            <div class="topcontent">
+                <div class="boxed" id="one">
+                    <div class="box_title" id="nametitle">
+                        Name
+                    </div>
+                    <div class="box_content" id="name">
+                        Adetomiwa Wesley
+                    </div>
+                </div>
+
+              <div class="boxed" id="two" >
+                    <div class="box_title">
+                        Matric number
+                    </div>
+                    <div class="box_content">
+                        19CH026485
+                    </div>
+                </div>
+
+                <div class="boxed">
+                    <div class="box_title">
+                        Hall/room number
+                    </div>
+                    <div class="box_content">
+                        John H105
+                    </div>
+                </div>
+            </div>
+
+            <div class="downcontent">
+                <div class="boxed">
+                    <div class="box_title">
+                        Registered courses
+                    </div>
+                    <div class="box_content">
+                        John H105
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </body>
+</html>
